58c8a95f1b6e9308a6f1414da0392d12ccbb0ba8
fsmardani <fatemehsmardani@gmail.com>
2021-12-07 07:18:23 +0330

    range label-pillow to django error

--- mlAmeri/app/admin.py
+++ mlAmeri/app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Input
+from .models import Input,Output
 # Register your models here.
 admin.site.register(Input)
+admin.site.register(Output)
--- mlAmeri/app/utils.py
+++ mlAmeri/app/utils.py
@@ -1,3 +1,13 @@
 def input_path(instance, filename):
     file_type = filename.split(".")[-1]
     return f"{instance.__class__.__name__}/{instance.id}.{file_type}"
+
+def coutput_path(instance, filename):
+    file_type = filename.split(".")[-1]
+    return f"{instance.__class__.__name__}/C{instance.id}.{file_type}"
+def moutput_path(instance, filename):
+    file_type = filename.split(".")[-1]
+    return f"{instance.__class__.__name__}/M{instance.id}.{file_type}"
+def cmoutput_path(instance, filename):
+    file_type = filename.split(".")[-1]
+    return f"{instance.__class__.__name__}/CM{instance.id}.{file_type}"
